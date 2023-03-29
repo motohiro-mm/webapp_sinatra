7caed5d11a0527c0097e69750fd04cd2a76be2aa
motohiro-mm <polarbear.dx@gmail.com>
2023-03-29 15:55:08 +0900

    メインファイルを修正

--- main_memo.rb
+++ main_memo.rb
@@ -5,108 +5,105 @@ require 'sinatra/reloader'
 require 'csv'
 require 'cgi'
 
+
 set :environment, :production
 
 MEMOPATH = File.realpath('memo_data.csv')
 IDPATH = File.realpath('id_data.csv')
 
-def memo_data
+def memos
   CSV.read(MEMOPATH, headers: true, header_converters: :symbol)
 end
 
-def id_max_plus1
-  id_data = CSV.read(IDPATH).to_a
-  id_max = id_data.map(&:join).map(&:to_i).max
-  id_max.nil? ? 1 : id_max + 1
-end
-
-def id_add(id)
+def new_id
+  id_max = CSV.read(IDPATH).last.join.to_i
+  update_id = id_max.nil? ? 1 : id_max + 1
   CSV.open(IDPATH, 'a') do |csv|
-    csv << [id]
+    csv << [update_id]
   end
+  update_id
 end
 
 def add_memo(title, content)
   CSV.open(MEMOPATH, 'a') do |csv|
-    csv << [title, content, id_max_plus1]
+    csv << [title, content, new_id]
   end
-  id_add(id_max_plus1)
 end
 
 def edit_memo(title, content, id)
-  current_data = memo_data
-  current_data.each do |c_d|
-    if c_d[:id] == id
-      c_d[:title] = title
-      c_d[:content] = content
+  current_memos = memos
+  current_memos.each do |current_memo|
+    if current_memo[:id] == id
+      current_memo[:title] = title
+      current_memo[:content] = content
     end
   end
-  overwrite_memo(current_data.to_a)
+  overwrite_memos(current_memos.to_a)
 end
 
 def delete_memo(id)
-  current_data = memo_data
-  current_data.delete_if do |c_d|
-    c_d[:id] == id
+  current_memos = memos
+  current_memos.delete_if do |current_memo|
+    current_memo[:id] == id
   end
 
-  overwrite_memo(current_data.to_a)
+  overwrite_memos(current_memos.to_a)
 end
 
-def overwrite_memo(new_data)
+def overwrite_memos(new_memos)
   CSV.open(MEMOPATH, 'w') do |csv|
-    new_data.each do |n_d|
-      csv << n_d
+    new_memos.each do |new_memo|
+      csv << new_memo
     end
   end
 end
 
-def sanitize(params_data)
-  CGI.escapeHTML(params_data)
+def sanitize(data)
+  CGI.escapeHTML(data)
 end
 
 get '/' do
-  redirect '/top_memo'
+  redirect '/memos'
 end
 
-get '/top_memo' do
-  @top_data = memo_data
+get '/memos' do
+  @top_memos = memos
 
   erb :top_memo
 end
 
-get '/new_memo' do
+get '/memos/new' do
   erb :new_memo
 end
 
 post '/create_memo' do
-  add_memo(sanitize(params[:new_title]), sanitize(params[:new_content]))
+  add_memo(params[:new_title], params[:new_content])
 
-  redirect '/top_memo'
+  redirect '/memos'
 end
 
-get '/show_memo/:id' do
+get '/memos/:id' do
   link_id = params[:id]
-  @link_data = memo_data.find { |d| d[:id] == link_id }
+  @link_memo = memos.find { |memo| memo[:id] == link_id }
 
   erb :show_memo
 end
 
-get '/edit_memo/:id' do
+get '/memos/:id/edit' do
   link_id = params[:id]
-  @link_data = memo_data.find { |d| d[:id] == link_id }
+  @link_memo = memos.find { |memo| memo[:id] == link_id }
 
   erb :edit_memo
 end
 
 patch '/edit_memo' do
-  edit_memo(sanitize(params[:edit_title]), sanitize(params[:edit_content]), params[:edit_id])
+  edit_memo(params[:edit_title], params[:edit_content], params[:edit_id])
 
-  redirect '/top_memo'
+  redirect '/memos'
 end
 
 delete '/delete_memo' do
   delete_memo(params[:delete_id])
 
-  redirect '/top_memo'
+  redirect '/memos'
 end
